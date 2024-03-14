c39366193be1dc3737be5cdb8fb08579080bb2d4
Kristofer <kristofer@fmckl.se>
2024-03-14 11:37:07 +0100

    Go back to using only one gql endpoint

--- src/graphql/eas.ts
+++
@@ -1,9 +0,0 @@
-import { initGraphQLTada } from "gql.tada";
-import { introspection } from "../graphql-eas-env";
-
-export const gqlEas = initGraphQLTada<{
-  introspection: introspection;
-}>();
-
-export type { FragmentOf, ResultOf, VariablesOf } from "gql.tada";
-export { readFragment } from "gql.tada";
--- src/graphql/hypercerts.ts
+++
@@ -1,9 +0,0 @@
-import { initGraphQLTada } from "gql.tada";
-import { introspection } from "../graphql-hypercerts-env";
-
-export const gqlHypercerts = initGraphQLTada<{
-  introspection: introspection;
-}>();
-
-export type { FragmentOf, ResultOf, VariablesOf } from "gql.tada";
-export { readFragment } from "gql.tada";
